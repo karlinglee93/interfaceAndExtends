d24ece23484c395fdab0313fde77e2283079b2ad
karlinglee93 <459607025@qq.com>
2018-08-03 10:56:25 +0800

    interface to implementation class

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -6,11 +6,20 @@ public class Hero {
 
 	public static void main(String[] args) {
 		ADHero ad = new ADHero();
-
-		// ad 指向的对象是ADHero类型，这个类型已经实现了AD接口
-		// 把ADHero类型的ad 转换成AD接口，
-		// 而AD接口中有physicAttack方法，而ADHero中一定有physicAttack方法的，
-		// 所以能够转换成功
+		
+		// ad 指向的对象是ADHero类型，adi 引用是AD接口类型。
+		// 将ad 转换为AD接口，是实现类转换为接口，是向上转换的
+		// 无需强制转换就会成功
 		AD adi = ad;
+
+		// adi 指向接口实现类ADHero，所以能够强制转换成功
+		ADHero adHero = (ADHero) adi;
+		
+		// adi 指向的对象adapHero 是ADAPHero类型，所以会转换失败。
+		// 注：虽然ADAPHero类型实现了AD接口，
+		// 但是假设能够转换成功，那么就可以使用magicAttack方法，
+		// 而adi引用所指向的对象ADHero是没有magicAttack方法的。
+		ADAPHero adapHero = (ADAPHero) adi;
+		adapHero.magicAttack();
 	}
 }
