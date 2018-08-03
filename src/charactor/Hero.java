7ec6878b4429fea0984ad43cc4966938ae3e4e66
karlinglee93 <459607025@qq.com>
2018-08-03 13:58:38 +0800

    use Polymorphism

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -1,5 +1,6 @@
 package charactor;
 
+import property.Item;
 import property.LifePotion;
 import property.MagicPotion;
 
@@ -7,22 +8,19 @@ public class Hero {
 	public String name;
 	public float hp;
 
-	public void useLifePotion(LifePotion lifePotion) {
-		lifePotion.effect();
+	public void useItem(Item item) {
+		item.effect();
 	}
-	
-	public void useMagicPotion(property.MagicPotion magicPotion) {
-		magicPotion.effect();
-	}
-	
+
 	public static void main(String[] args) {
 		Hero hero = new Hero();
 		hero.name = "盖伦";
-		
+
 		LifePotion lifePotion = new LifePotion();
 		MagicPotion magicPotion = new MagicPotion();
-		
-		hero.useLifePotion(lifePotion);
-		hero.useMagicPotion(magicPotion);
+
+		// 传入Item 类的子类对象的引用
+		hero.useItem(lifePotion);
+		hero.useItem(magicPotion);
 	}
 }
