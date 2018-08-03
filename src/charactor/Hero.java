5066967a987d0bc531347963c0a409fcc13639b7
karlinglee93 <459607025@qq.com>
2018-08-03 10:25:26 +0800

    fater to child

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -7,16 +7,17 @@ public class Hero {
 	public static void main(String[] args) {
 		Hero h = new Hero();
 		ADHero ad = new ADHero();
-		
-		// 类型转换指的是把一个引用所指向的对象的类型，转换为另一个引用的类型
+		Support s = new Support();
 
-		// 把ad引用所指向的对象的类型是ADHero
-		// h引用的类型是Hero
-		
-		// 方法：把右边的当作左边的来用
-		
-		// 把ADHero当做Hero使用，一定可以
-		// 即所有的子类转父类都是说得通的
+		// 从右至左，子类转父类完全可以，此时h指向ADHero ad对象
 		h = ad;
+		// 所有再将ADHero ad对象的h 强制转换成ADHero ad对象 也不会报错
+		ad = (ADHero) h;
+		// 此时h指向Support s对象
+		h = s;
+		// 将Support s对象的h 强制转换成ADHero ad对象 会报错
+		// 因为s对象与ad对象不同，且不是父子关系
+		ad = (ADHero) h;
+		
 	}
 }
