d1115cc3e412b99c7aa5e4ce16c3210900216cca
karlinglee93 <459607025@qq.com>
2018-08-03 11:01:52 +0800

    instanceof

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -6,20 +6,13 @@ public class Hero {
 
 	public static void main(String[] args) {
 		ADHero ad = new ADHero();
+		APHero ap = new APHero();
 		
-		// ad 指向的对象是ADHero类型，adi 引用是AD接口类型。
-		// 将ad 转换为AD接口，是实现类转换为接口，是向上转换的
-		// 无需强制转换就会成功
-		AD adi = ad;
-
-		// adi 指向接口实现类ADHero，所以能够强制转换成功
-		ADHero adHero = (ADHero) adi;
+		Hero h1 = ad;
+		Hero h2 = ap;
 		
-		// adi 指向的对象adapHero 是ADAPHero类型，所以会转换失败。
-		// 注：虽然ADAPHero类型实现了AD接口，
-		// 但是假设能够转换成功，那么就可以使用magicAttack方法，
-		// 而adi引用所指向的对象ADHero是没有magicAttack方法的。
-		ADAPHero adapHero = (ADAPHero) adi;
-		adapHero.magicAttack();
+		System.out.println(h1 instanceof ADHero);
+		System.out.println(h2 instanceof APHero);
+		System.out.println(h1 instanceof Hero);
 	}
 }
