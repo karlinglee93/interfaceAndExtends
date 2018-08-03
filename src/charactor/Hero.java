2f31858bdad5102cb2118225031280e74aab091a
karlinglee93 <459607025@qq.com>
2018-08-03 10:00:18 +0800

    child to fater

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -2,31 +2,21 @@ package charactor;
 
 public class Hero {
 	private String name;
-	private int hp;
-	private int armor;
+	private float hp;
 
-	public String getName() {
-		return name;
+	public static void main(String[] args) {
+		Hero h = new Hero();
+		ADHero ad = new ADHero();
+		
+		// 类型转换指的是把一个引用所指向的对象的类型，转换为另一个引用的类型
+
+		// 把ad引用所指向的对象的类型是ADHero
+		// h引用的类型是Hero
+		
+		// 方法：把右边的当作左边的来用
+		
+		// 把ADHero当做Hero使用，一定可以
+		// 即所有的子类转父类都是说得通的
+		h = ad;
 	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getHp() {
-		return hp;
-	}
-
-	public void setHp(int hp) {
-		this.hp = hp;
-	}
-
-	public int getArmor() {
-		return armor;
-	}
-
-	public void setArmor(int armor) {
-		this.armor = armor;
-	}
-
 }
