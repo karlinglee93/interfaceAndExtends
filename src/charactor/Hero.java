633bdc968ba3002d11e19a539440e6c1d081cfe5
karlinglee93 <459607025@qq.com>
2018-08-06 10:22:48 +0800

    Object - equals()

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -1,6 +1,5 @@
 package charactor;
 
-// Object类是所有类的父类
 public class Hero {
 	String name;
 	float hp;
@@ -9,21 +8,23 @@ public class Hero {
 		return name;
 	}
 
-	public void finalize() {
-		System.out.println("这个英雄正在被回收");
+	public boolean equals(Object object) {
+		if (object instanceof Hero) {
+			Hero hero = (Hero) object;
+			return this.hp == hero.hp;
+		}
+		return false;
 	}
 
 	public static void main(String[] args) {
-		// 只有一个引用
-		Hero hero;
+		Hero h1 = new Hero();
+		h1.hp = 300;
+		Hero h2 = new Hero();
+		h2.hp = 400;
+		Hero h3 = new Hero();
+		h3.hp = 300;
 
-		// 不断生成新的对象
-		// 每创建一个对象，前一个对象就没有引用指向了
-		// 那些对象，就满足垃圾回收的条件
-		// 当垃圾堆积的比较多的时候，就会触发垃圾回收
-		// 一旦这个对象被回收，它的finalize()方法就会被调用
-		for (int i = 0; i < 5; i++) {
-			hero = new Hero();
-		}
+		System.out.println(h1.equals(h2));
+		System.out.println(h1.equals(h3));
 	}
 }
