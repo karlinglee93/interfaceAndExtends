b6818bc93478341e47757926073e149108c7b9ea
karlinglee93 <459607025@qq.com>
2018-08-06 10:08:41 +0800

    Object - toString()

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -1,31 +1,20 @@
 package charactor;
 
-import property.Item;
-import property.LifePotion;
-import property.MagicPotion;
-
-public class Hero {
+// Object类是所有类的父类
+public class Hero extends Object {
 	String name;
 	float hp;
-	float armor;
-	int moveSpeed;
 
-	public void useItem(Item item) {
-		System.out.println("hero use item");
-		item.effect();
-	}
-
-	// 准备显式提供无参构造方法的父类
-	public Hero() {
-		System.out.println("Hero的构造方法");
-	}
-
-	// 带一个参数的构造方法
-	public Hero(String name) {
-		System.out.println("Hero的有一个参数的构造方法");
+	public String toString() {
+		return name;
 	}
 
 	public static void main(String[] args) {
-		new Hero();
+		Hero hero = new Hero();
+		hero.name = "safari";
+		// Object类提供一个toString方法，所以所有的类都有toString方法
+		System.out.println(hero.toString());
+		// 直接打印对象就是打印该对象的toString()返回值
+		System.out.println(hero);
 	}
 }
