63db40ddaa81077aa89bbd1be669f5141934c624
karlinglee93 <459607025@qq.com>
2018-08-06 10:14:36 +0800

    Object - finalize()

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -1,7 +1,7 @@
 package charactor;
 
 // Object类是所有类的父类
-public class Hero extends Object {
+public class Hero {
 	String name;
 	float hp;
 
@@ -9,12 +9,21 @@ public class Hero extends Object {
 		return name;
 	}
 
+	public void finalize() {
+		System.out.println("这个英雄正在被回收");
+	}
+
 	public static void main(String[] args) {
-		Hero hero = new Hero();
-		hero.name = "safari";
-		// Object类提供一个toString方法，所以所有的类都有toString方法
-		System.out.println(hero.toString());
-		// 直接打印对象就是打印该对象的toString()返回值
-		System.out.println(hero);
+		// 只有一个引用
+		Hero hero;
+
+		// 不断生成新的对象
+		// 每创建一个对象，前一个对象就没有引用指向了
+		// 那些对象，就满足垃圾回收的条件
+		// 当垃圾堆积的比较多的时候，就会触发垃圾回收
+		// 一旦这个对象被回收，它的finalize()方法就会被调用
+		for (int i = 0; i < 5; i++) {
+			hero = new Hero();
+		}
 	}
 }
