d74598e6b9bc74f72625f11d3f361b0e0d7e4cc0
karlinglee93 <459607025@qq.com>
2018-08-06 09:09:30 +0800

    类的静态方法 - 隐藏(new)

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -8,28 +8,9 @@ public class Hero {
 	public String name;
 	public float hp;
 
-	public void useItem(Item item) {
-		item.effect();
-	}
-	
-	public void kill(Mortal m) {
-		m.die();
-	}
-
-	public static void main(String[] args) {
-		Hero hero = new Hero();
-		hero.name = "盖伦";
-
-		ADHero adHero = new ADHero();
-		APHero apHero = new APHero();
-		ADAPHero adapHero = new ADAPHero();
-		
-		// kill() 方法需要传入Mortal接口类型
-		// 由于ADHero/APHero/ADAPHero类实现了Mortal接口
-		// 传入ADHero/APHero/ADAPHero类的引用也就是允许发生的
-		// 因为类的引用到实现的接口是向上转型的关系
-		hero.kill(adHero);
-		hero.kill(apHero);
-		hero.kill(adapHero);
+	//类方法，静态方法
+	//通过类就可以直接调用
+	public static void battleWin() {
+		System.out.println("hero battle win! ");
 	}
 }
