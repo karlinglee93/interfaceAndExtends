b7729e6e3abc3ca3699b99ee83a14282317308fd
karlinglee93 <459607025@qq.com>
2018-08-07 09:53:15 +0800

    InnerClass - static

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -6,30 +6,27 @@ public class Hero {
 	float armor;
 	int moveSpeed;
 
-	class BattleScore {
-		int kill;
-		int die;
-		int assit;
-
-		public void legendary() {
-			if (kill >= 8) {
-				// 作为Hero的非静态内部类，是可以直接访问外部类的private实例属性name的
-				System.out.println(name + "超神");
-			} else {
-				System.out.println(name + "尚未超神！");
-			}
-		}
+	private static void battleWin() {
+		System.out.println("battle win");
 	}
 
-	public static void main(String[] args) {
-		Hero hero = new Hero();
-		hero.name = "safari";
+	public static class EnemyCrystal {
+		int hp = 5000;
 
-		// 实例化内部类
-		// BattleScore对象只有在一个英雄对象存在的时候才有意义
-		// 所以其实例化必须建立在一个外部类对象的基础之上
-		BattleScore battleScore = hero.new BattleScore();
-		battleScore.kill = 8;
-		battleScore.legendary();
+		// 如果水晶的血量为0，则宣布胜利
+		public void checkIfVictory() {
+			if (hp == 0) {
+				Hero.battleWin();
+			}
+
+			// 静态内部类不能直接访问外部类的对象属性
+			System.out.println(name + "win this game");
+		}
+
+		public static void main(String[] args) {
+			// 实例化静态内部类
+			Hero.EnemyCrystal enemyCrystal = new Hero.EnemyCrystal();
+			enemyCrystal.checkIfVictory();
+		}
 	}
 }
