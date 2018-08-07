d948d88d8fec0020fd41bc9b5b1cb7c28fabc681
karlinglee93 <459607025@qq.com>
2018-08-07 10:31:23 +0800

    InnerClass - local class

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -9,20 +9,16 @@ public abstract class Hero {
 	public abstract void attack();
 
 	public static void main(String[] args) {
-		ADHero adh = new ADHero();
-		// 通过打印adh，可以看到adh这个对象属于ADHero类
-		adh.attack();
-		System.out.println(adh);
+		// 与匿名类的区别在于，本地类有了自定义的类名
+		class SomeHero extends Hero {
 
-		// 匿名类
-		Hero h = new Hero() {
-			// 当场实现attack方法
+			@Override
 			public void attack() {
-				System.out.println("新的进攻手段");
+				System.out.println(name + " 新的进攻手段");
 			}
-		};
-		h.attack();
-		// 通过打印h，可以看到h这个对象属于Hero$1这么一个系统自动分配的类名
-		System.out.println(h);
+		}
+		SomeHero someHero = new SomeHero();
+		someHero.name = "safari";
+		someHero.attack();
 	}
 }
