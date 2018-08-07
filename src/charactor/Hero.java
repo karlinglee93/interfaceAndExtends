5db598c730bcab845e52f95d13862d1864e66570
karlinglee93 <459607025@qq.com>
2018-08-07 09:22:27 +0800

    InnerClass - non_static

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -1,16 +1,35 @@
 package charactor;
 
-public abstract class Hero {
+public class Hero {
 	String name;
 	float hp;
 	float armor;
 	int moveSpeed;
 
-	// 抽象方法attack
-	// Hero的子类会被要求实现attack方法
-	public abstract void attack();
+	class BattleScore {
+		int kill;
+		int die;
+		int assit;
+
+		public void legendary() {
+			if (kill >= 8) {
+				// 作为Hero的非静态内部类，是可以直接访问外部类的private实例属性name的
+				System.out.println(name + "超神");
+			} else {
+				System.out.println(name + "尚未超神！");
+			}
+		}
+	}
 
 	public static void main(String[] args) {
+		Hero hero = new Hero();
+		hero.name = "safari";
 
+		// 实例化内部类
+		// BattleScore对象只有在一个英雄对象存在的时候才有意义
+		// 所以其实例化必须建立在一个外部类对象的基础之上
+		BattleScore battleScore = hero.new BattleScore();
+		battleScore.kill = 8;
+		battleScore.legendary();
 	}
 }
